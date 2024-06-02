60289eb5f23d6b8368e95c1743296a98af8074b6
Jashesh Kanzariya <153159733+jasheshK@users.noreply.github.com>
2024-06-02 18:34:10 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,17 +14,17 @@ df = pandas.read_csv("data.csv")
 
 with col1:
     for index, row in df[:4].iterrows():
-        st.header(f"{row["first name"].title()} {row["last name"].title()}")
+        st.header(f'{row["first name"].title()} {row["last name"].title()}')
         st.write(row["role"])
         st.image("images/" + row["image"])
 with col2:
     for index, row in df[4:8].iterrows():
-        st.header(f"{row["first name"].title()} {row["last name"].title()}")
+        st.header(f'{row["first name"].title()} {row["last name"].title()}')
         st.write(row["role"])
         st.image("images/" + row["image"])
 with col3:
     for index, row in df[8:].iterrows():
-        st.header(f"{row["first name"].title()} {row["last name"].title()}")
+        st.header(f'{row["first name"].title()} {row["last name"].title()}')
         st.write(row["role"])
         st.image("images/" + row["image"])
 
